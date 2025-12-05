b5c551ee295bab183c76f983795a06f494a7a6b4
Can Miquel <user@canmiquel.com>
2025-12-05 14:03:25 +0100

    Update weekly field labels to show usage/waste

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Can Miquel Inventory PWA
-const CACHE_NAME = 'can-miquel-inventory-v3';
+const CACHE_NAME = 'can-miquel-inventory-v4';
 const ASSETS = [
     './',
     './index.html',
